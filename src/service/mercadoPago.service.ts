48f1086eca9996c4361ba176124b82f56c166376
fedeberon <federicoberon@outlook.com>
2025-12-23 17:03:45 -0300

    fix: Update Mercado Pago access token to use hardcoded value for testing

--- src/service/mercadoPago.service.ts
+++ src/service/mercadoPago.service.ts
@@ -12,7 +12,7 @@ if (!MP_ACCESS_TOKEN) {
   // eslint-disable-next-line no-console
   console.warn('MP_ACCESS_TOKEN no está configurado; los pagos de Mercado Pago fallarán.');
 } else {
-  mpClient = new MercadoPagoConfig({ accessToken: MP_ACCESS_TOKEN });
+  mpClient = new MercadoPagoConfig({ accessToken: 'APP_USR-4340936701059728-122020-0fafe420cbdce897714b056d8d770bae-3081058712' });
   prefClient = new Preference(mpClient);
   paymentClient = new Payment(mpClient);
 }
